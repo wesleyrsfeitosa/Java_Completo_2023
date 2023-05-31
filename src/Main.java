0e226112498068b60a9b7ad879954eb649694aa1
wesleyrsfeitosa <wesleyrsfeitosa@yahoo.com.br>
2023-05-31 11:45:51 -0300

    Salvando Mudanças

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
+		Scanner teclado = new Scanner(System.in);
+		
 		System.out.println("Olá Mundo!");
+		int num1, num2;
+	
+		
+		System.out.println("Entre com 1º numero");
+		num1 = teclado.nextInt();
+		
+		System.out.println("Entre com 2º numero");
+		num2 = teclado.nextInt();
+		
+		int soma = num1 + num2;
+		
+		System.out.println("A soma de num1 + num2 é: " + soma);
 
 	}
 
